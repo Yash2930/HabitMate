97b16a36c123a013ac6fa6ea25c770a3e31292db
Yashwardhan <yashv1062@gmail.com>
2023-09-27 00:41:25 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,16 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import WeekView from "./WeekView";
 
 const App = () => {
-  
-  
   return (
-    <>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/week-view" element={<WeekView/>}/>
-    </Routes>
-    </>
+    <BrowserRouter basename="/HabitMate">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/week-view" element={<WeekView />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -9,10 +9,8 @@ import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
       <Provider store={store}>
         <App />
       </Provider>
-    </Router>
   </React.StrictMode>
 );
